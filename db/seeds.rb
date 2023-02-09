cc8353eda00131404e736c72254b52378514eaf5
carlyyburton <carlyyburton@gmail.com>
2023-02-09 13:10:07 -0600

    Products controller created.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+676.times do
+  new_product = Product.new( title: Faker::Commerce.unique.product_name,
+                             price: Faker::Commerce.price,
+                             stock_quantity: Faker::Number)
+  new_product.save
+end
