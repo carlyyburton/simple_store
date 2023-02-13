fe462c6243391a028317b5038adf90bd3c9070d5
carlyyburton <carlyyburton@gmail.com>
2023-02-13 13:24:40 -0600

    Category model added.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   get 'products/index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get '/products/:id', to: 'products#show', as: 'product'
   root 'products#index'
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :products
 end
